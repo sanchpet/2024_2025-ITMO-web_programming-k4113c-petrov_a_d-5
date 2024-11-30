096c5d8f14b467ef6c199ca6af4e83facd86e5b8
sanchpet <petrov@petrovad.ru>
2024-11-30 14:40:50 +0300

    Initial commit

---
+++ precache-manifest.aca947889ed09ca6b45b96477c8e65bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760085c3371870a3c7964c60da4b26c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "789ee85e162f010c4aca",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "789ee85e162f010c4aca",
+    "url": "/static/js/main.16549cac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
